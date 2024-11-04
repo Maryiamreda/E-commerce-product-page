ea20244967b5e6406475427699871135fecc0753
Maryiamreda <maryiamreda@gmail.com>
2024-11-04 21:56:23 +0200

    app

---
+++ src/components/navbar/navBar.tsx
@@ -0,0 +1,32 @@
+import './navBar.module.scss';
+import Logo from '../../../public/images/logo.svg';
+import Cart from '../../../public/images/icon-cart.svg';
+import Avatar from '../../../public/images/image-avatar.png';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div style={{ display: "inline-flex" }}>
+                <div style={{ paddingTop: "14px" }}>
+                    <img src={Logo} width={140} height={20} />
+                </div>
+                <ul>
+                    <li>Collections</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>About</li>
+                    <li>Contact</li>
+
+                </ul></div>
+            <div style={{ display: "flex", gap: "25px" }}>
+                <img src={Cart} width={20} height={20} style={{ padding: "13px" }} />
+                <img src={Avatar} width={40} height={40} />
+            </div>
+        </nav>
+
+
+
+    );
+}
+
+export default NavBar;
