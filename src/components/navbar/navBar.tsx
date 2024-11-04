a886788e43c1bb3668ae54e7574b57ad8176ccd0
Maryiamreda <maryiamreda@gmail.com>
2024-11-05 00:15:41 +0200

    adding hover effects

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -1,28 +1,34 @@
 import './navBar.module.scss';
+import styles from './navBar.module.scss';
+
 import Logo from '../../../public/images/logo.svg';
 import Cart from '../../../public/images/icon-cart.svg';
 import Avatar from '../../../public/images/image-avatar.png';
 
 const NavBar = () => {
     return (
-        <nav>
-            <div style={{ display: "inline-flex" }}>
-                <div style={{ paddingTop: "14px" }}>
-                    <img src={Logo} width={140} height={20} />
-                </div>
-                <ul>
-                    <li>Collections</li>
-                    <li>Men</li>
-                    <li>Women</li>
-                    <li>About</li>
-                    <li>Contact</li>
+        <>
+            <nav>
+                <div style={{ display: "inline-flex" }}>
+                    <div style={{ paddingTop: "14px" }}>
+                        <img src={Logo} width={140} height={20} />
+                    </div>
+                    <ul>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contact</li>
 
-                </ul></div>
-            <div style={{ display: "flex", gap: "25px" }}>
-                <img src={Cart} width={20} height={20} style={{ padding: "13px" }} />
-                <img src={Avatar} width={40} height={40} />
-            </div>
-        </nav>
+                    </ul></div>
+                <div style={{ display: "flex", gap: "25px" }}>
+                    <img src={Cart} width={20} height={20} style={{ padding: "13px" }} />
+                    <img src={Avatar} width={50} height={50} className={styles.avatar} />
+                </div>
+            </nav>
+            <hr />
+        </>
+
 
 
 
