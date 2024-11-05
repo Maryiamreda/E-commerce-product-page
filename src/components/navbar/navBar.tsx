0a97719d078376aef280f4f314a52ad5641f4460
Maryiamreda <maryiamreda@gmail.com>
2024-11-05 15:36:33 +0200

    adding Sidebar for the mobile design

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -4,16 +4,27 @@ import styles from './navBar.module.scss';
 import Logo from '../../../public/images/logo.svg';
 import Cart from '../../../public/images/icon-cart.svg';
 import Avatar from '../../../public/images/image-avatar.png';
+import Menu from '../../../public/images/icon-menu.svg';
+import CloseButton from '../../../public/images/icon-close.svg';
+
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
     return (
         <>
             <nav>
                 <div style={{ display: "inline-flex" }}>
-                    <div style={{ paddingTop: "14px" }}>
+                    <div style={{ paddingTop: "14px", display: "inline-flex", gap: "13px" }}>
+                        <img src={Menu} width={20} height={22} className={styles.menuIcon} onClick={toggleMobileMenu} />
+
                         <img src={Logo} width={140} height={20} />
                     </div>
-                    <ul>
+                    <ul className={styles.desktopNav}>
                         <li>Collections</li>
                         <li>Men</li>
                         <li>Women</li>
@@ -27,6 +38,18 @@ const NavBar = () => {
                 </div>
             </nav>
             <hr />
+            {isMobileMenuOpen && (
+                <div className={styles.mobileMenu}>
+                    <img src={CloseButton} width={20} height={20} className={styles.closeButton} onClick={toggleMobileMenu} />
+                    <ul>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            )}
         </>
 
 
